b0dd406a2ac0b9e3b61935d5226e7c5e3ee71cad
Chukwunwike Udezue <udezuec@protonmail.com>
2024-01-20 22:04:33 +0100

    Fix: Contact us page metadata missing

--- app/contact_us/page.tsx
+++ app/contact_us/page.tsx
@@ -1,5 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Contact Us - Alt Educational Consult Limited",
+  description: "Keep in touch with us",
+  keywords: [""],
+  openGraph: {
+    title: "Contact Us - Alt Educational Consult Limited",
+    description: "Keep in touch with us",
+  },
+  twitter: {
+    title: "Contact Us - Alt Educational Consult Limited",
+    description: "Keep in touch with us",
+  },
+};
 
 function ContactUsPage() {
   return (
@@ -54,7 +69,12 @@ function ContactUsPage() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
